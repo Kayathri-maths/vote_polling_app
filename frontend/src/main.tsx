aab97d533ded1dda9d8fb827a186f5f6c97a602d
Kayathri-maths <kayualagarsamy@gmail.com>
2025-12-09 20:52:02 +0530

    removed unwanted lines

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,43 +1,10 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import CreatePoll from "./pages/CreatePoll";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
-import Register from "./pages/Register";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route
-        path="/dashboard"
-        element={
-          // <ProtectedRoute>
-          <Dashboard />
-          // </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/create"
-        element={
-          // <ProtectedRoute>
-          <CreatePoll />
-          // </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/profile"
-        element={
-          // <ProtectedRoute>
-          <Profile />
-          // </ProtectedRoute>
-        }
-      />
-    </Routes>
+    <App />
   </BrowserRouter>
 );
